972d1149873dc3ef6a5ecf7725f30df687cbe2e6
Amr Shipl <amrshipl09@gmail.com>
2026-03-18 04:55:51 +0000

    Force single page access

--- logistics/views/__init__.py
+++ logistics/views/__init__.py
@@ -1,4 +1,1 @@
-from .auth_views import RegisterDelegateView
-# تأكد إن عندك ملف اسمه store_views.py جوه مجلد views وفيه الكلاس ده
-from .store_views import StoreListView 
-
+# ملف فارغ مؤقتاً لضمان التشغيل
