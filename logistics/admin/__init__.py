42c2ddd8385cb80a389424528de31d269181a015
aksab-git <finance@aksab.shop>
2026-03-21 04:32:09 +0200

    Update __init__.py

--- logistics/admin/__init__.py
+++ logistics/admin/__init__.py
@@ -3,16 +3,19 @@ from django.contrib.auth.models import User
 from .sales_admin import CustomUserAdmin
 from ..models.sales_rep import SalesRepresentative
 from ..models.sales_manager import SalesManager
-from ..models.transactions import StockTransfer, TransferItem # استيراد الموديلات الجديدة
 
-# 1. إعادة تسجيل المستخدمين بصلاحيات مخصصة
+# استيراد كلاسات الـ Admin من الملفات الفرعية
+# تأكد إن الأسماء هنا مطابقة تماماً للي فوق
+from .inventory_admin import WarehouseAdmin, InventoryItemAdmin, StockTransferAdmin
+
+# 1. تنظيف وتسجيل مستخدمين النظام
 try:
     admin.site.unregister(User)
 except admin.sites.NotRegistered:
     pass
 admin.site.register(User, CustomUserAdmin)
 
-# 2. تسجيل مندوبي المبيعات والمديرين (تجنب التكرار)
+# 2. تسجيل مندوبي المبيعات والمديرين
 try:
     admin.site.unregister(SalesRepresentative)
 except admin.sites.NotRegistered:
@@ -25,6 +28,5 @@ except admin.sites.NotRegistered:
     pass
 admin.site.register(SalesManager)
 
-# 3. استيراد تسجيلات المخازن والتحويلات من الملفات الفرعية
-# السطر ده هينده على inventory_admin اللي إحنا لسه مصلحينه
-from .inventory_admin import WarehouseAdmin, InventoryItemAdmin, StockTransferAdmin
+# ملاحظة: تم تسجيل Warehouse, InventoryItem, و StockTransfer 
+# داخل ملف inventory_admin.py باستخدام الـ Decorator @admin.register
