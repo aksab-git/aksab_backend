1a3e56b6605ecf69123ec110aef85cdb00961bd9
Amr Shipl <amrshipl09@gmail.com>
2026-03-21 01:21:38 +0000

    Fix delivery dashboard navigation: linked add products and view offers screens

--- logistics/views/__init__.py
+++ logistics/views/__init__.py
@@ -4,4 +4,4 @@ from .work_day_views import WorkDayAPIView
 
 # كده أي ملف بره مجلد views يقدر يستدعيهم مباشرة
 from .stockView import MyInventoryViewSet, MyTransfersViewSet
-
+from .loadReqest import StockTransferViewSet 
